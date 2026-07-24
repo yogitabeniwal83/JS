558cbea9df308bfe4e6eeb684ff902a872c705e9
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-07-24 11:11:31 +0530

    Add error handling js file

---
+++ Error_Handling/script.js
@@ -0,0 +1,15 @@
+let a = 5;
+let b = 10;
+console.log("a = ", a);
+console.log("b = ", b);
+console.log("a+b = ", a+b);
+try {
+console.log("a+b = ", a+c);     // error
+} catch(err) {
+    console.log(err);
+}
+
+console.log("a+b = ", a+b);
+console.log("a+b = ", a+b);
+console.log("a+b = ", a+b);
+console.log("a+b = ", a+b);
