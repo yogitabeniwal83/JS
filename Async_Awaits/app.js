5a9b93c226c4f088d7a06a9d026c682cadecfb4a
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-07-26 10:41:12 +0530

    Add async await aap.js file

---
+++ Async_Awaits/app.js
@@ -0,0 +1,15 @@
+function api() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+             console.log("weather data");
+             resolve(200);
+        }, 2000);
+    });
+}
+
+async function getWeatherData() {
+     await api();  // 1st
+     await api();   // 2nd
+}
+
+getWeatherData();
