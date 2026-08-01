85a57735d5426bf91d68c724f82426bd4b2fcf7b
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-08-01 09:35:02 +0530

    Add file

---
+++ DOM/script.js
@@ -0,0 +1,31 @@
+let firstElements = document.querySelector("p");    // 1st element
+console.dir(firstElements);
+
+let allElements = document.querySelectorAll("p");   // all element
+console.dir(allElements);
+
+
+
+let firstElement = document.querySelector(".heading-class");    // 1st element
+console.dir(firstElement);
+
+let allElement = document.querySelectorAll(".heading-class");   // all element
+console.dir(allElement);
+
+
+let firstEl = document.querySelector("#id");    // 1st element
+console.dir(firstEl);
+
+// querySelectorAll by id not exit   
+let allEl = document.querySelectorAll("#id");   // all element
+console.dir(allEl);
+
+
+// let heading = document.getElementById("heading");
+
+// console.dir(heading);
+
+
+// let headings = document.getElementsByClassName("heading-class");
+// console.log(headings);
+// console.dir(headings);
