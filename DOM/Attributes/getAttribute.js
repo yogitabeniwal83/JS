0e06feffa52a7c937f03988b04eee1a1b68f329e
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-08-07 10:53:23 +0530

    Add file

---
+++ DOM/Attributes/getAttribute.js
@@ -0,0 +1,13 @@
+let div = document.querySelector("div");
+console.log(div);
+
+let id = div.getAttribute("id");
+console.log(id);
+
+let name = div.getAttribute("name");
+console.log(name);
+
+
+
+let para = document.querySelector("p");
+console.log(para.getAttribute("class"));
