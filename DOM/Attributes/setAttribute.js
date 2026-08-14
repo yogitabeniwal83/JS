54470ca51ddcc643f3375e54b80097879f83afba
yogitabeniwal83 <yogitabeniwal83@gmail.com>
2026-08-14 20:37:53 +0530

    Add file

---
+++ DOM/Attributes/setAttribute.js
@@ -0,0 +1,12 @@
+let para = document.querySelector("p");
+console.log(para.setAttribute("class", "newClass"));
+
+
+
+let div = document.querySelector("div");
+console.log(div); 
+
+div.style.backgroundColor = "purple";
+div.style.fontSize = "26px";
+
+div.innerText = "Hello";   
